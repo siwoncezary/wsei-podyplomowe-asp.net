4f75aac8d974bac84326115c793add1960fd44c0
Cezary Siwoń <siwon.cezary@gmail.com>
2024-02-04 09:54:10 +0100

    Book Create.cshtml

--- Z2/Models/Book.cs
+++ Z2/Models/Book.cs
@@ -10,15 +10,19 @@ public class Book
     [Required]
     [MinLength(3)]
     [MaxLength(50)]
+    [Display(Name = "Tytuł książki")]
     public string Title { get; set; }
     [Required]
     [MinLength(5)]
     [MaxLength(50)]
+    [Display(Name = "Autor książki")]
     public string Author { get; set; }
+    [Display(Name = "Liczba stron")]
     [Range(1,2000)]
     public int Pages { get; set; }
     [StringLength(13)]
     public string ISBN { get; set; }
     [DataType(DataType.Date)]
+    [Display(Name = "Data wydania")]
     public DateOnly Published { get; set; }
 }
