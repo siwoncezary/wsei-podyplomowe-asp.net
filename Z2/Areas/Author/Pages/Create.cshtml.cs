f4f2f19b8637e508b94a0b6838121e8bac61da97
Cezary Siwoń <siwon.cezary@gmail.com>
2024-02-04 17:19:21 +0100

    RazorPage Author/Create fixed

--- Z2/Areas/Author/Pages/Create.cshtml.cs
+++ Z2/Areas/Author/Pages/Create.cshtml.cs
@@ -1,21 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Z2.Areas.Author.Pages;
-
-public class Create : PageModel
+namespace Z2.Areas.Author.Pages
 {
 
-    [BindProperty] 
-    public  Model.Author Author { get; set; }
-    
-    public void OnGet()
+    public class Create : PageModel
     {
-        Author = new Model.Author() { FirstName = "", LastName = "" };
-    }
+        // Model widoku formularza
+        public class Author
+        {
+            [Required]
+            public string FirstName { get; set; }
+            [Required]
+            public string LastName { get; set; }
+        }
+        
+        [BindProperty] 
+        public Author Input { get; set; }
 
-    public IActionResult OnPost()
-    {
-        return RedirectToAction("Index","Book");
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                // model encji utworzonej na podstawie modelu widoku
+                var authorEntity = new Model.Author()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName
+                };
+                // tu powinien znaleźć się kod zapisania w aplikacji autora
+                
+                // Przykładowe przekierowanie do kontrolere i metody
+                return RedirectToAction("Index", "Book");
+            }
+            // zwrócenie bieżącego widoku RazorPage, czyli formularza Create z komunikatami błędów  
+            return Page();
+        }
     }
 }
