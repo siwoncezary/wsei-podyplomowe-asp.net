b530bf333a909c4389bf80e2eb72c25a34739363
Cezary Siwoń <siwon.cezary@gmail.com>
2024-02-04 11:31:53 +0100

    IBookService.cs

---
+++ Z2/Models/IBookService.cs
@@ -0,0 +1,10 @@
+﻿namespace Z2.Models;
+
+public interface IBookService
+{
+    void Add(Book book);
+    IEnumerable<Book> FindAll();
+    void Delete(int id);
+    void Edit(Book book);
+    Book? FindById(int id);
+}
---
+++ Z2/Models/ListBookService.cs
@@ -0,0 +1,44 @@
+﻿namespace Z2.Models;
+
+public class ListBookService: IBookService
+{
+    private readonly List<Book> _books = new List<Book>();
+    private int _id = 0;
+
+    public void Add(Book book)
+    {
+        book.Id = ++_id;
+        _books.Add(book);
+    }
+
+    public IEnumerable<Book> FindAll()
+    {
+        return _books;
+    }
+
+    public void Delete(int id)
+    {
+        Book? bookToDelete = FindById(id);
+        if (bookToDelete is not null)
+        {
+            _books.Remove(bookToDelete);
+        }
+    }
+
+    public void Edit(Book book)
+    {
+        Book? bookToEdit = FindById(book.Id);
+        if (bookToEdit is not null)
+        {
+            _books.Remove(bookToEdit);
+            _books.Add(book);
+        }
+        
+    }
+
+    public Book? FindById(int id)
+    {
+        return _books
+            .FirstOrDefault(book => book.Id == id);
+    }
+}
