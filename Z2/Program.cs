261926b234ce7dac7d16b49575bf1ad86dd113b0
Cezary Siwoń <siwon.cezary@gmail.com>
2024-02-25 17:00:07 +0100

    end of day 4

--- Z2/Program.cs
+++ Z2/Program.cs
@@ -15,14 +15,14 @@ public class Program
         // Add services to the container.
         builder.Services.AddRazorPages();       // dodać
         builder.Services.AddControllersWithViews();
-        // builder.Services.AddDbContext<AppDbContext>(options =>
-        // {
-        //     options.UseSqlite(connectionString);
-        // });
         builder.Services.AddDbContext<AppDbContext>(options =>
         {
-            options.UseSqlServer(connectionStringMS);
+            options.UseSqlite(connectionString);
         });
+        // builder.Services.AddDbContext<AppDbContext>(options =>
+        // {
+        //     options.UseSqlServer(connectionStringMS);
+        // });
         
         builder.Services.AddDefaultIdentity<IdentityUser>(options =>        // dodać
         {
@@ -33,22 +33,23 @@ public class Program
         .AddRoles<IdentityRole>()
         .AddEntityFrameworkStores<AppDbContext>();
         builder.Services.AddTransient<IBookService, EFBookService>();
-
+        builder.Services.AddSession();
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+/*        if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
-        }
+        }*/
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
 
         app.UseRouting();
-
+        app.UseSession();
+        app.UseAuthentication();
         app.UseAuthorization();
         app.MapRazorPages();               
         app.MapControllerRoute(
